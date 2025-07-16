eeddc1d288d03e03485935d2336506e18ad96721
ABHISHEKGUPTA-1234 <abhi8929314120@gmail.com>
2025-07-16 23:45:41 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client' for React 18+
-import './index.css'; // You might have a default index.css, or create one if needed
-import App from './App'; // Import your App component
-
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
